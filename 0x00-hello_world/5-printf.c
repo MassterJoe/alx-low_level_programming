9abaf69baaa9153364c052f8fcaa55a309668dbf
Salawu O Joseph <104222651+MassterJoe@users.noreply.github.com>
2022-09-08 23:45:45 +0100

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+# include <stdio>
+/** main - entry point
+*Description - with proper grammar, but the outcome is a piece of art,
+*Return: always 0 (success)
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
+
+ 
+
