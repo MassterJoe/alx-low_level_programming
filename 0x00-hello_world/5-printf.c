72028e4b3f9aebb4e7b0a2ec7c3005b1882a73f4
Salawu O Joseph <104222651+MassterJoe@users.noreply.github.com>
2022-09-08 23:51:03 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-/** main - entry point
+/**
+*main - entry point
 *Description - with proper grammar, but the outcome is a piece of art,
 *Return: always 0 (success)
 */
@@ -9,6 +10,7 @@ int main(void)
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return(0);
 }
+
 
  
 
