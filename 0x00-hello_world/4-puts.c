260b3a2ed0cb249bb86022d6ef3643c935ae3674
Salawu O Joseph <104222651+MassterJoe@users.noreply.github.com>
2022-09-08 23:18:34 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 # include <stdio.h>
-/* this is a code for put */
+/* this is a code for put
+ *betty comment style
+ *betty
+ */
+
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
