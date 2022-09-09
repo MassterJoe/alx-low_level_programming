7a1b9166fbbe90a64fe2146f8b8b41eddec81b4e
Salawu O. Joseph <sojbaba98@gmail.com>
2022-09-09 16:19:07 -0700

    msg

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description - prints combinations of single digit
+ *
+ *Return: Always 0 (success)
+ */
