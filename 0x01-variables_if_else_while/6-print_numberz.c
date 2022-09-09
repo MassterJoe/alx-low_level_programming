27636c8cb5e04ded21e3db7e9716df06aefe8fb8
Salawu O. Joseph <sojbaba98@gmail.com>
2022-09-09 14:49:03 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description -Write a program that prints decimal numbers
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+putchar((n % 10) + '0');
+{
+putchar('\n')
+}
+return (0);
+}
