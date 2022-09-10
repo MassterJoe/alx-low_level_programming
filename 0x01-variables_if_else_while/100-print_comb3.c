7aa239c57c15c8bf69e013505cea8df9ba385ce9
Salawu O. Joseph <sojbaba98@gmail.com>
2022-09-10 00:25:14 -0700

    msg

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,9 +11,9 @@
 int main(void)
 {
 int n1, n2;
-for (n1 = 0; n1 < 10; n1++)
+for (n1 = 0; n1 <= 9; n1++)
 {
-for (n2 = 0; n2 < 10; n2++)
+for (n2 = 1; n2 <= 9; n2++)
 {
 putchar((n1 % 10) + '0');
 putchar((n2 % 10) + '0');
