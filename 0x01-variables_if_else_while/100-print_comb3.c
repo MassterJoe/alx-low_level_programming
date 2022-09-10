b0683f9d848044ac634ca540a56c5f43add559a8
Salawu O. Joseph <sojbaba98@gmail.com>
2022-09-09 23:48:41 -0700

    printbnumbers from 0-99 with spaces

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,9 +2,28 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *main - Entry point
- *
- *Description - prints combinations of single digit
- *
- *Return: Always 0 (success)
- */
+*main - Entry point
+*
+*Description - prints numbers from 0 to 99 separated by commas
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int n1, n2;
+for (n1 = 0; n1 < 10; n1++)
+{
+for (n2 = 0; n2 < 10; n2++)
+{
+putchar((n1 % 10) + '0');
+putchar((n2 % 10) + '0');
+if ((n1 == 9) && (n2 == 9))
+continue;
+putchar(',');
+purchar(' ');
+
+}
+}
+putchar('\n');
+return (0);
+}
