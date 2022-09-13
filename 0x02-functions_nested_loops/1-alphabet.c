6f1ee9585b93826b9b72ed9b48b70dd5fec7b07d
Salawu O. Joseph <sojbaba98@gmail.com>
2022-09-13 15:44:07 -0700

    ksj

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
 *main - Entry point
-*print_alphabet -  prints alphsbets in lower case
-*
+*@print_alphabet(): prints alphsbets in lower case
+*@int : integer
+*@_putchar() : for printing
+*@print_alphabet() : for printing alphabet
 *Return: Always 0 (Succes)
 */
-int _putchar(int n);
+int _putchar(int);
 void print_alphabet(void);
 void print_alphabet(void)
 {
