91eeb3426d8d148428d91c8693fc13ee3f9550e4
Salawu O. Joseph <sojbaba98@gmail.com>
2022-09-13 15:21:55 -0700

    hju

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,6 @@
 *Return: Always 0 (Succes)
 */
 int _putchar(int n);
-void print_alphabet(void);
 void print_alphabet(void)
 {
 int n;
@@ -16,7 +15,7 @@ _putchar(n);
 }
 _putchar('\n');
 }
-print_alphabet();
+void print_alphabet(void);
 int main(void)
 {
 print_alphabet();
