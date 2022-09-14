bc32c634cd4d548e3c80672be91e8a0cc9075f2b
Salawu O. Joseph <sojbaba98@gmail.com>
2022-09-14 01:06:06 -0700

    msg

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,10 @@
-#include "main.h"
+r#include "main.h"
 /**
 *main - Entry point
-*@int : integer
-*@_putchar() : for printing
-*@print_alphabet() : for printing alphabet
-*Return: Always 0 (Succes)
+*@n : integer
+*@print_alphabet() : function
+*Return: Always 0 (Success)
 */
-int _putchar(int);
-void print_alphabet(void);
 void print_alphabet(void)
 {
 int n;
@@ -16,10 +13,4 @@ for (n = 'a'; n <= 'z'; n++)
 _putchar(n);
 }
 _putchar('\n');
-}
-int main(void)
-void print_alphabet(void);
-{
-print_alphabet();
-return (0);
 }
